54bd908b31d3d92634c85470aea49c7a6fdf19fe
Douglas White <email@douglaswhite.dev>
2024-07-08 13:38:52 -0500

    fix scroll icon
    Signed-off-by: Douglas White <email@douglaswhite.dev>

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from "@angular/core";
-import { IonContent } from "@ionic/angular";
+import { IonContent, ScrollCustomEvent } from "@ionic/angular";
 
 @Component({
   selector: "app-home",
@@ -8,26 +8,15 @@ import { IonContent } from "@ionic/angular";
 })
 export class HomePage {
   showBackToTopFab = false;
-
   @ViewChild(IonContent, { static: false })
   content!: IonContent;
-  scrollEnd = false;
-  isShown: any;
 
   onScrollEnd(ev: Event) {
-    this.scrollEnd = true;
     this.showBackToTopFab =
-      (ev as CustomEvent).detail.scrollTop > 250 ? true : false;
-  }
-
-  scrollToId(id: any) {
-    const element: any = document.getElementById(id);
-    this.content.scrollToPoint(0, element.offsetTop - 45, 750);
+      (ev as ScrollCustomEvent).detail.scrollTop > 450 ? true : false;
   }
 
   scrollToTop() {
-    this.content.scrollToTop(750).then(() => {
-      this.scrollEnd = false;
-    });
+    this.content.scrollToTop(750).then(() => {});
   }
 }
